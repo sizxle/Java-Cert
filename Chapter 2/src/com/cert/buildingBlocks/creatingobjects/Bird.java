c1d1e1a6704830dfac5cfceda6bf24a2f3a90309
Sthembizo Molefi <sthembizomolefi@gmail.com>
2025-11-06 00:53:21 +0200

    work

---
+++ Chapter 2/src/com/cert/buildingBlocks/creatingobjects/Bird.java
@@ -0,0 +1,10 @@
+package com.cert.buildingBlocks.creatingobjects;
+
+public class Bird {
+
+    public static void main(String[] args){
+        {System.out.println("hello world");}
+        new Bird();
+    }
+    {System.out.println("in object creation");}
+}
