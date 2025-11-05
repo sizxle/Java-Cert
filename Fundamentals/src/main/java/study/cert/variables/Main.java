bdd1a4b171aedc6a46424c191e543ffb3771d708
Sthembizo Molefi <sthembizomolefi@gmail.com>
2025-11-06 00:42:40 +0200

    variable study

--- Fundamentals/src/main/java/study/cert/variables/Main.java
+++ Fundamentals/src/main/java/study/cert/variables/Main.java
@@ -1,8 +1,19 @@
 package study.cert.variables;
 
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
         int age= 30 ; //. stores data in computers memory
+
+        int y=age;
+        age=26;
+        System.out.println(age);
+        System.out.println(y);
+        System.out.print("hello \"world\" ");
     }
+
 }
